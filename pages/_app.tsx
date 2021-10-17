7f0b80c1d55fcaf86ec30c6b9327fceb1a612eaa
mimshins <mostafa.sh.coderino@gmail.com>
2021-10-17 19:37:32 +0330

    [:tada:] setup `nextjs` and `sonnat/ui`

---
+++ pages/_app.tsx
@@ -0,0 +1,89 @@
+import CssBaseline from "@sonnat/ui/CssBaseline";
+import PageLoader from "@sonnat/ui/PageLoader";
+import makeStyles from "@sonnat/ui/styles/makeStyles";
+import SonnatInitializer from "@sonnat/ui/styles/SonnatInitializer";
+import Head from "next/head";
+import * as React from "react";
+import smoothScroll from "smoothscroll-polyfill";
+import { usePageState } from "store";
+import type { AppPropsWithLayout } from "types";
+import { setTitleMeta } from "utils";
+
+import "public/static/fonts.css";
+
+const useGlobalStyles = makeStyles(
+  {
+    "@global": {
+      "html, body": { scrollBehavior: "smooth" },
+      img: { verticalAlign: "middle" },
+      a: { textDecoration: "none", color: "currentcolor" }
+    }
+  },
+  { name: "GlobalStyles" }
+);
+
+const App = (props: AppPropsWithLayout): React.ReactNode => {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  const { Component: Page, pageProps, router } = props;
+
+  useGlobalStyles();
+
+  const getPageLayout =
+    Page.getLayout || (() => (page: React.ReactNode) => page);
+
+  const withPageLayout = getPageLayout();
+
+  const isPageLoading = usePageState(state => state.isPageLoading);
+  const setPageLoading = usePageState(state => state.setPageLoading);
+
+  React.useEffect(() => {
+    const routeChangeStart = () => setPageLoading(true);
+    const routeChangeComplete = () => setPageLoading(false);
+
+    router.events.on("routeChangeStart", routeChangeStart);
+    router.events.on("routeChangeComplete", routeChangeComplete);
+    router.events.on("routeChangeError", routeChangeComplete);
+
+    return () => {
+      router.events.off("routeChangeStart", routeChangeStart);
+      router.events.off("routeChangeComplete", routeChangeComplete);
+      router.events.off("routeChangeError", routeChangeComplete);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  React.useEffect(() => {
+    const sonnatServerStyles = document.getElementById("sonnat-jss-ssr");
+
+    if (sonnatServerStyles)
+      sonnatServerStyles.parentElement?.removeChild(sonnatServerStyles);
+  }, []);
+
+  React.useEffect(() => void smoothScroll.polyfill(), []);
+
+  return (
+    <SonnatInitializer injectFirst>
+      <Head>
+        {setTitleMeta(
+          "Porser | Porser is a Persian open-source web application that specializes in building online form, surveys, quizzes and polls."
+        )}
+        <meta
+          name="viewport"
+          content="initial-scale=1.0, width=device-width, maximum-scale=5.0, minimum-scale=1.0"
+          key="viewport"
+        />
+      </Head>
+      <div id="main-wrapper">
+        <CssBaseline />
+        {withPageLayout(
+          <React.Fragment>
+            <PageLoader loading={isPageLoading} />
+            <Page {...pageProps} />
+          </React.Fragment>
+        )}
+      </div>
+    </SonnatInitializer>
+  );
+};
+
+export default App;
---
+++ pages/_document.tsx
@@ -0,0 +1,100 @@
+import ServerStyleSheets from "@sonnat/ui/styles/ServerStyleSheets";
+import AutoPrefixer from "autoprefixer";
+import CleanCss from "clean-css";
+import Document, {
+  DocumentContext,
+  Head,
+  Html,
+  Main,
+  NextScript
+} from "next/document";
+import PostCss from "postcss";
+import * as React from "react";
+
+const prefixer = PostCss([AutoPrefixer]);
+const cleaner = new CleanCss();
+
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const sheets = new ServerStyleSheets();
+
+    const originalRenderPage = ctx.renderPage;
+
+    ctx.renderPage = () =>
+      originalRenderPage({
+        enhanceApp: App => props => sheets.collect(<App {...props} />)
+      });
+
+    const initialProps = await Document.getInitialProps(ctx);
+
+    const css = sheets.toString();
+    const sheetId = sheets.getStyleElementId();
+
+    const minifiedCSS = await (async rawCSS => {
+      // It might be undefined, e.g. after an error.
+      if (rawCSS) {
+        return cleaner.minify(
+          (await prefixer.process(rawCSS, { from: undefined })).css
+        ).styles;
+      } else return rawCSS;
+    })(css);
+
+    return {
+      ...initialProps,
+      // Styles fragment is rendered after the app and page rendering finish.
+      styles: [
+        ...React.Children.toArray(initialProps.styles),
+        <style key={sheetId} id={sheetId}>
+          {minifiedCSS}
+        </style>
+      ]
+    };
+  }
+
+  render(): JSX.Element {
+    return (
+      <Html lang="en-US">
+        <Head>
+          <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+          <meta property="robots" content="Index, Follow" />
+          <meta name="mobile-web-app-capable" content="yes" />
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <meta name="google" content="notranslate" />
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="og:image" content="/meta-facebook-image.png" />
+          <meta name="twitter:image" content="/meta-twitter-image.png" />
+          <meta
+            name="twitter:image:alt"
+            content="Porser is a Persian open-source web application that specializes in building online form, surveys, quizzes and polls."
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+          <meta name="application-name" content="Porser" />
+          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#ea475b" />
+          <meta name="msapplication-TileColor" content="#27184f" />
+          <meta name="theme-color" content="#27184f" />
+          <link rel="manifest" href="/site.webmanifest" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
