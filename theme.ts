76d0f920c08eb38dd9edfcce8deeedda03d4b411
mimshins <mostafa.sh.coderino@gmail.com>
2021-11-13 11:07:09 +0330

    [:star:] create custom theme

---
+++ theme.ts
@@ -0,0 +1,8 @@
+import createTheme from "@sonnat/ui/styles/createTheme";
+
+const theme = createTheme({
+  colors: { primary: "#f56468", secondary: "#27184f" },
+  custom: {}
+});
+
+export default theme;
