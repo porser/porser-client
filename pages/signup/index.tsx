4b71d2fb3847c730795fbe4ff9722855e0777030
mimshins <mostafa.sh.coderino@gmail.com>
2021-11-22 19:33:25 +0330

    [:sparkles:] implement the signup page

--- pages/signup/index.tsx
+++ pages/signup/index.tsx
@@ -1,33 +1,65 @@
 import makeStyles from "@sonnat/ui/styles/makeStyles";
 import SignupForm from "components/form/SignupForm";
 import AuthLayout from "components/layout/AuthLayout";
+import type { GetServerSideProps } from "next";
+import Head from "next/head";
 import * as React from "react";
 import type { Layout, NextPageWithLayout } from "types";
+import {
+  renderErrorPage,
+  setTitleMeta,
+  withServerSideAuthConfirmation
+} from "utils";
+import type { ErrorType } from "utils/useConfirmAuthentication";
 
 const useStyles = makeStyles(
   theme => {
     const {
-      colors,
       typography: { pxToRem }
     } = theme;
 
-    return { root: {}, form: {} };
+    return {
+      root: {
+        height: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center"
+      },
+      form: { maxWidth: pxToRem(512) }
+    };
   },
-  { name: "SignupPage" }
+  { name: "Page" }
 );
 
-const SignupPage: NextPageWithLayout = () => {
+interface Props {
+  preRenderErrors?: {
+    globalError: ErrorType;
+    connectionError: ErrorType;
+  };
+}
+
+const Page: NextPageWithLayout<Props> = props => {
+  const { preRenderErrors } = props;
+
   const classes = useStyles();
 
+  if (preRenderErrors) return renderErrorPage(preRenderErrors);
+
   return (
-    <section className={classes.root}>
-      <SignupForm className={classes.form} />
-    </section>
+    <React.Fragment>
+      <Head>{setTitleMeta("PORSER | ثبت‌نام")}</Head>
+      <section className={classes.root}>
+        <SignupForm className={classes.form} />
+      </section>
+    </React.Fragment>
   );
 };
 
 const PageLayout: Layout = page => <AuthLayout>{page}</AuthLayout>;
 
-SignupPage.getLayout = () => PageLayout;
+export const getServerSideProps: GetServerSideProps = async context =>
+  withServerSideAuthConfirmation(context);
 
-export default SignupPage;
+Page.getLayout = () => PageLayout;
+
+export default Page;
