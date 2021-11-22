f487bf56ee98d9cd632323cee40619637135a4b6
mimshins <mostafa.sh.coderino@gmail.com>
2021-11-22 19:28:12 +0330

    [:sparkles:] implement the signup form

--- components/form/SignupForm/SignupForm.tsx
+++ components/form/SignupForm/SignupForm.tsx
@@ -1,5 +1,20 @@
+import { Eye, EyeO } from "@sonnat/icons";
+import {
+  Button,
+  FormControl,
+  FormControlFeedback,
+  InputAdornment,
+  Text,
+  TextField
+} from "@sonnat/ui";
+import makeUserAPI from "apis/user.api";
 import c from "classnames";
+import Link from "next/link";
+import { useRouter } from "next/router";
 import * as React from "react";
+import { useAuthState } from "store";
+import { storeRefreshToken, validateEmailByBrowser } from "utils";
+import feathersClient from "utils/feathers/client";
 import useStyles from "./styles";
 
 interface SignupFormBaseProps {
@@ -7,21 +22,295 @@ interface SignupFormBaseProps {
 }
 
 type SignupFormProps = Omit<
-  React.ComponentPropsWithRef<"div">,
+  React.ComponentPropsWithRef<"form">,
   keyof SignupFormBaseProps
 > &
   SignupFormBaseProps;
 
+interface Field<T> {
+  value: T;
+  error: string;
+  isValid: boolean;
+}
+
+type State = {
+  email: Field<string>;
+  password: Field<string>;
+};
+
+type Action =
+  | { type: "SET_VALUE"; name: keyof State; value: string }
+  | { type: "SET_ERROR"; name: keyof State; error: string }
+  | { type: "SET_VALIDITY"; name: keyof State; validity: boolean };
+
+type Reducer = (prevState: State, action: Action) => State;
+
+const INITIAL_STATE: State = {
+  email: { value: "", error: "", isValid: false },
+  password: { value: "", error: "", isValid: false }
+};
+
+const reducer: Reducer = (prevState, action) => {
+  switch (action.type) {
+    case "SET_VALUE": {
+      const { name, value } = action;
+      return { ...prevState, [name]: { ...prevState[name], value } };
+    }
+    case "SET_ERROR": {
+      const { name, error } = action;
+      return { ...prevState, [name]: { ...prevState[name], error } };
+    }
+    case "SET_VALIDITY": {
+      const { name, validity } = action;
+      return {
+        ...prevState,
+        [name]: { ...prevState[name], isValid: validity }
+      };
+    }
+    default:
+      return prevState;
+  }
+};
+
+const checkFormValidity = (
+  inputs: State
+): Partial<Record<keyof State, string>> | null => {
+  const isEmailValid = validateEmailByBrowser(inputs.email.value);
+  const isPasswordValid = inputs.password.value.length >= 10;
+
+  if (isPasswordValid && isEmailValid) return null;
+
+  const errors: Partial<Record<keyof State, string>> = {};
+
+  if (!isPasswordValid)
+    errors.password = "رمز عبور غیر معتبر می‌باشد. (حداقل ۱۰ کاراکتر)";
+  if (!isEmailValid) errors.email = "ایمیل غیر معتبر می‌باشد.";
+
+  return errors;
+};
+
+const UserAPI = makeUserAPI(feathersClient);
+
 const SignupFormBase = (
   props: SignupFormProps,
-  ref: React.Ref<HTMLDivElement>
+  ref: React.Ref<HTMLFormElement>
 ) => {
   const { className, ...otherProps } = props;
 
   const classes = useStyles();
+  const router = useRouter();
+
+  const [isSubmitting, setIsSubmitting] = React.useState(false);
+  const [globalError, setGlobalError] = React.useState("");
+
+  const [isSubmitActive, setIsSubmitActive] = React.useState(false);
+  const [isPasswordVisible, setIsPasswordVisible] = React.useState(false);
+  const [passwordInputType, setPasswordInputType] = React.useState<
+    "password" | "text"
+  >("password");
+
+  const setUser = useAuthState(state => state.setUser);
+
+  const [inputs, dispatch] = React.useReducer(reducer, INITIAL_STATE);
+
+  const showErrors = (errors: Partial<Record<keyof State, string>> | null) => {
+    dispatch({
+      type: "SET_ERROR",
+      name: "email",
+      error: (errors && errors.email) || ""
+    });
+    dispatch({
+      type: "SET_ERROR",
+      name: "password",
+      error: (errors && errors.password) || ""
+    });
+  };
+
+  const submit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    setGlobalError("");
+
+    const errors = checkFormValidity(inputs);
+    void showErrors(errors);
+
+    if (errors) return;
+
+    setIsSubmitting(true);
+
+    const registerResponse = await UserAPI.register(
+      inputs.email.value,
+      inputs.password.value
+    );
+
+    if (registerResponse.error && registerResponse.errors) {
+      setIsSubmitting(false);
+
+      if (registerResponse.errors.global) {
+        setGlobalError(registerResponse.errors.global);
+      } else if (registerResponse.errors.connection) {
+        setGlobalError(registerResponse.errors.connection);
+      } else {
+        if (registerResponse.errors.email) {
+          dispatch({
+            type: "SET_ERROR",
+            name: "email",
+            error: registerResponse.errors.email
+          });
+
+          dispatch({
+            type: "SET_VALIDITY",
+            name: "email",
+            validity: false
+          });
+        } else if (registerResponse.errors.password) {
+          dispatch({
+            type: "SET_ERROR",
+            name: "password",
+            error: registerResponse.errors.password
+          });
+
+          dispatch({
+            type: "SET_VALIDITY",
+            name: "password",
+            validity: false
+          });
+        }
+      }
+    } else {
+      const loginResponse = await UserAPI.login(
+        inputs.email.value,
+        inputs.password.value
+      );
+
+      if (loginResponse.error && loginResponse.errors) {
+        setIsSubmitting(false);
+        if (loginResponse.errors.global) {
+          setGlobalError(loginResponse.errors.global);
+        } else if (loginResponse.errors.auth) {
+          setGlobalError(loginResponse.errors.auth);
+        } else if (loginResponse.errors.connection) {
+          setGlobalError(loginResponse.errors.connection);
+        }
+      } else {
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion, @typescript-eslint/no-unused-vars
+        const { refreshToken, ...userData } = loginResponse.data!;
+
+        setUser(userData);
+        storeRefreshToken(refreshToken);
+
+        void router.replace("/dashboard");
+      }
+    }
+  };
+
+  const handleTextChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { value, name } = e.target;
+
+    if (value !== inputs[name as keyof State].value) {
+      dispatch({
+        type: "SET_VALIDITY",
+        name: name as keyof State,
+        validity: !!value
+      });
+
+      dispatch({
+        type: "SET_VALUE",
+        name: name as keyof State,
+        value
+      });
+    }
+  };
+
+  const handlePasswordVisibility = () => {
+    setPasswordInputType(isPasswordVisible ? "password" : "text");
+    setIsPasswordVisible(!isPasswordVisible);
+  };
+
+  React.useEffect(() => {
+    setIsSubmitActive(
+      Object.keys(inputs).every(key => inputs[key as keyof State].isValid)
+    );
+  }, [inputs]);
 
   return (
-    <div ref={ref} className={c(className, classes.root)} {...otherProps}></div>
+    <form
+      ref={ref}
+      onSubmit={e => void submit(e)}
+      className={c(className, classes.root)}
+      {...otherProps}
+    >
+      {globalError && <div className={classes.formError}>{globalError}</div>}
+      <Text variant="h6" rootNode="h1" className={classes.title}>
+        به پُرسِر خوش‌آمدید!
+      </Text>
+      <Text variant="bodySmall" rootNode="p" color="textSecondary">
+        برای ثبت‌نام، ایمیل و رمز عبور خود را وارد کنید:
+      </Text>
+      <FormControl
+        className={classes.formControl}
+        fluid
+        required
+        hasError={!!inputs.email.error}
+      >
+        <TextField
+          placeholder="ایمیل"
+          name="email"
+          size="large"
+          value={inputs.email.value}
+          onChange={handleTextChange}
+        />
+        {inputs.email.error && (
+          <FormControlFeedback>{inputs.email.error}</FormControlFeedback>
+        )}
+      </FormControl>
+      <FormControl
+        className={classes.formControl}
+        fluid
+        required
+        hasError={!!inputs.password.error}
+      >
+        <TextField
+          placeholder="رمز عبور"
+          type={passwordInputType}
+          name="password"
+          size="large"
+          value={inputs.password.value}
+          onChange={handleTextChange}
+          trailingAdornment={
+            <InputAdornment
+              variant="icon"
+              onClick={() => void handlePasswordVisibility()}
+            >
+              {!isPasswordVisible ? <EyeO /> : <Eye />}
+            </InputAdornment>
+          }
+        />
+        {inputs.password.error && (
+          <FormControlFeedback>{inputs.password.error}</FormControlFeedback>
+        )}
+      </FormControl>
+      <Button
+        className={classes.submitBtn}
+        type="submit"
+        color="primary"
+        label="ثبت‌نام"
+        size="large"
+        loading={isSubmitting}
+        disabled={!isSubmitActive}
+      />
+      <Text
+        variant="bodySmall"
+        rootNode="p"
+        color="textSecondary"
+        className={classes.footer}
+      >
+        <span>حساب کاربری دارم:</span>
+        <Link href="/login">
+          <a title="ورود به حساب کاربری">ورود</a>
+        </Link>
+      </Text>
+    </form>
   );
 };
 
--- components/form/SignupForm/styles.ts
+++ components/form/SignupForm/styles.ts
@@ -4,10 +4,45 @@ const useStyles = makeStyles(
   theme => {
     const {
       colors,
-      typography: { pxToRem }
+      typography: { pxToRem, variants }
     } = theme;
 
-    return { root: {} };
+    return {
+      root: {
+        display: "flex",
+        flexDirection: "column",
+        flex: "1 1",
+        padding: pxToRem(64),
+        backgroundColor: "rgba(255, 255, 255, 0.7)",
+        borderRadius: pxToRem(32),
+        backdropFilter: "blur(60px)"
+      },
+      formError: {
+        ...variants.bodySmall,
+        padding: `${pxToRem(8)} ${pxToRem(12)} ${pxToRem(8)} ${pxToRem(8)}`,
+        borderRadius: `${pxToRem(4)} 0 0 ${pxToRem(4)}`,
+        borderRight: `4px solid ${colors.pallete.red[900]}`,
+        color: colors.pallete.red[900],
+        whiteSpace: "pre-wrap",
+        backgroundColor: colors.pallete.pink[50],
+        marginBottom: pxToRem(16)
+      },
+      title: { marginBottom: pxToRem(8) },
+      formControl: { marginTop: pxToRem(16) },
+      submitBtn: { marginTop: pxToRem(32) },
+      footer: {
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        marginTop: pxToRem(8),
+        "& > a": {
+          color: colors.pallete.lightBlue[500],
+          transition: "color 240ms ease",
+          "&:hover": { color: colors.pallete.lightBlue[900] }
+        },
+        "& > span + a": { marginRight: pxToRem(8) }
+      }
+    };
   },
   { name: "SignupForm" }
 );
