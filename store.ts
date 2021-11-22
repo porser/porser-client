6e9519726300c1cccd63b2f949ca42b7a6de9843
mimshins <mostafa.sh.coderino@gmail.com>
2021-11-22 19:33:53 +0330

    [:sparkles:] add auth states

--- store.ts
+++ store.ts
@@ -1,9 +1,29 @@
+import type { User } from "types";
 import createStore, { State } from "zustand";
+import { persist } from "zustand/middleware";
 
 interface PageState extends State {
   isPageLoading: boolean;
   setPageLoading: (isLoading: boolean) => void;
 }
+
+interface AuthState extends State {
+  user: User | null;
+  setUser: (user: User | null) => void;
+}
+
+export const useAuthState = createStore<AuthState>(
+  persist(
+    set => ({
+      user: null,
+      setUser: user => set(() => ({ user }))
+    }),
+    {
+      name: "porser-auth-state",
+      partialize: state => ({ user: state.user })
+    }
+  )
+);
 
 export const usePageState = createStore<PageState>(set => ({
   isPageLoading: false,
