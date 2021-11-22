7f348fb9ece3c9ce3bae38c07148792d46174c31
mimshins <mostafa.sh.coderino@gmail.com>
2021-11-22 19:31:33 +0330

    [:star:] add forgot password page

---
+++ pages/forgot-password/index.tsx
@@ -0,0 +1,65 @@
+import makeStyles from "@sonnat/ui/styles/makeStyles";
+import ForgotPasswordForm from "components/form/ForgotPasswordForm";
+import AuthLayout from "components/layout/AuthLayout";
+import type { GetServerSideProps } from "next";
+import Head from "next/head";
+import * as React from "react";
+import type { Layout, NextPageWithLayout } from "types";
+import {
+  renderErrorPage,
+  setTitleMeta,
+  withServerSideAuthConfirmation
+} from "utils";
+import type { ErrorType } from "utils/useConfirmAuthentication";
+
+const useStyles = makeStyles(
+  theme => {
+    const {
+      typography: { pxToRem }
+    } = theme;
+
+    return {
+      root: {
+        height: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center"
+      },
+      form: { maxWidth: pxToRem(512) }
+    };
+  },
+  { name: "Page" }
+);
+
+interface Props {
+  preRenderErrors?: {
+    globalError: ErrorType;
+    connectionError: ErrorType;
+  };
+}
+
+const Page: NextPageWithLayout<Props> = props => {
+  const { preRenderErrors } = props;
+
+  const classes = useStyles();
+
+  if (preRenderErrors) return renderErrorPage(preRenderErrors);
+
+  return (
+    <React.Fragment>
+      <Head>{setTitleMeta("PORSER | فراموشی رمز عبور")}</Head>
+      <section className={classes.root}>
+        <ForgotPasswordForm className={classes.form} />
+      </section>
+    </React.Fragment>
+  );
+};
+
+const PageLayout: Layout = page => <AuthLayout>{page}</AuthLayout>;
+
+export const getServerSideProps: GetServerSideProps = async context =>
+  withServerSideAuthConfirmation(context);
+
+Page.getLayout = () => PageLayout;
+
+export default Page;
