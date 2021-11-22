c6f5bdb6df3e5f0881c70853c11b1cea02a65b37
mimshins <mostafa.sh.coderino@gmail.com>
2021-11-22 19:34:43 +0330

    [:star:] add constants

---
+++ constants.app.ts
@@ -0,0 +1,23 @@
+export const ACCESS_TOKEN_COOKIE_KEY = "porser-access-token";
+export const REFRESH_TOKEN_COOKIE_KEY = "porser-refresh-token";
+
+export const SERVER_ERROR = [
+  "مشکلی در سرور بوجود آمده است.",
+  "لطفاً کمی بعد دوباره امتحان کنید."
+].join("\n");
+
+export const CONNECTION_ERROR = [
+  "مشکلی در ارتباط با سرور پیش آمده؛",
+  "ممکن است دسترسی شما به اینترنت قطع شده و یا اینکه سرعت اینترنت کند باشد.",
+  "لطفاً کمی بعد دوباره امتحان کنید."
+].join("\n");
+
+export const NOT_FOUND_ERROR = "صفحه مورد نظر شما یافت نشد.";
+
+export const USER_NOT_VERIFIED = [
+  "حساب‌ کاربری شما تأیید نشده است.",
+  "برای تأیید کردن حساب‌ کاربریتان به ایمیلی که به این منظور، پیش‌تر برایتان ارسال شده، مراجعه کنید."
+].join("\n");
+
+export const INVALID_LINK_ERROR =
+  "لینک معتبر نمی‌باشد؛ ممکن است منقضی شده باشد.";
