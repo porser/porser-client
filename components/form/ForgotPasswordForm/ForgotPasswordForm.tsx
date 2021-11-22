42e01c828c9f9152632e2bc3394e7f4f3c2077ed
mimshins <mostafa.sh.coderino@gmail.com>
2021-11-22 19:28:56 +0330

    [:star:] implement reset password form

---
+++ components/form/ForgotPasswordForm/ForgotPasswordForm.tsx
@@ -0,0 +1,262 @@
+import {
+  Button,
+  FormControl,
+  FormControlFeedback,
+  Text,
+  TextField
+} from "@sonnat/ui";
+import makePwdManagementAPI from "apis/pwd-management.api";
+import c from "classnames";
+import Link from "next/link";
+import * as React from "react";
+import { validateEmailByBrowser } from "utils";
+import feathersClient from "utils/feathers/client";
+import useStyles from "./styles";
+
+interface ForgotPasswordFormBaseProps {
+  className?: string;
+}
+
+type ForgotPasswordFormProps = Omit<
+  React.ComponentPropsWithRef<"form">,
+  keyof ForgotPasswordFormBaseProps
+> &
+  ForgotPasswordFormBaseProps;
+
+interface Field<T> {
+  value: T;
+  error: string;
+  isValid: boolean;
+}
+
+type State = { email: Field<string> };
+
+type Action =
+  | { type: "RESET" }
+  | { type: "SET_VALUE"; name: keyof State; value: string }
+  | { type: "SET_ERROR"; name: keyof State; error: string }
+  | { type: "SET_VALIDITY"; name: keyof State; validity: boolean };
+
+type Reducer = (prevState: State, action: Action) => State;
+
+const INITIAL_STATE: State = {
+  email: { value: "", error: "", isValid: false }
+};
+
+const reducer: Reducer = (prevState, action) => {
+  switch (action.type) {
+    case "SET_VALUE": {
+      const { name, value } = action;
+      return { ...prevState, [name]: { ...prevState[name], value } };
+    }
+    case "SET_ERROR": {
+      const { name, error } = action;
+      return { ...prevState, [name]: { ...prevState[name], error } };
+    }
+    case "SET_VALIDITY": {
+      const { name, validity } = action;
+      return {
+        ...prevState,
+        [name]: { ...prevState[name], isValid: validity }
+      };
+    }
+    case "RESET":
+      return INITIAL_STATE;
+    default:
+      return prevState;
+  }
+};
+
+const checkFormValidity = (
+  inputs: State
+): Partial<Record<keyof State, string>> | null => {
+  const isEmailValid = validateEmailByBrowser(inputs.email.value);
+
+  if (isEmailValid) return null;
+
+  const errors: Partial<Record<keyof State, string>> = {};
+
+  if (!isEmailValid) errors.email = "ایمیل غیر معتبر می‌باشد.";
+
+  return errors;
+};
+
+const pwdManagementAPI = makePwdManagementAPI(feathersClient);
+
+const ForgotPasswordFormBase = (
+  props: ForgotPasswordFormProps,
+  ref: React.Ref<HTMLFormElement>
+) => {
+  const { className, ...otherProps } = props;
+
+  const classes = useStyles();
+
+  const [isSubmitting, setIsSubmitting] = React.useState(false);
+  const [globalError, setGlobalError] = React.useState("");
+  const [successMessage, setSuccessMessage] = React.useState("");
+
+  const [isSubmitActive, setIsSubmitActive] = React.useState(false);
+
+  const [inputs, dispatch] = React.useReducer(reducer, INITIAL_STATE);
+
+  const showErrors = (errors: Partial<Record<keyof State, string>> | null) => {
+    dispatch({
+      type: "SET_ERROR",
+      name: "email",
+      error: (errors && errors.email) || ""
+    });
+  };
+
+  const submit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    setGlobalError("");
+    setSuccessMessage("");
+
+    const errors = checkFormValidity(inputs);
+    void showErrors(errors);
+
+    if (errors) return;
+
+    setIsSubmitting(true);
+
+    const response = await pwdManagementAPI.sendResetPasswordEmail(
+      inputs.email.value
+    );
+
+    setIsSubmitting(false);
+
+    if (response.error && response.errors) {
+      if (response.errors.global) {
+        setGlobalError(response.errors.global);
+      } else if (response.errors.connection) {
+        setGlobalError(response.errors.connection);
+      } else {
+        if (response.errors.email) {
+          dispatch({
+            type: "SET_ERROR",
+            name: "email",
+            error: response.errors.email
+          });
+
+          dispatch({
+            type: "SET_VALIDITY",
+            name: "email",
+            validity: false
+          });
+        } else if (response.errors.skip) {
+          setSuccessMessage(response.errors.skip);
+        }
+      }
+    } else {
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      setSuccessMessage(response.data!.message);
+    }
+  };
+
+  const handleTextChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { value, name } = e.target;
+
+    if (value !== inputs[name as keyof State].value) {
+      dispatch({
+        type: "SET_VALIDITY",
+        name: name as keyof State,
+        validity: !!value
+      });
+
+      dispatch({
+        type: "SET_VALUE",
+        name: name as keyof State,
+        value
+      });
+    }
+  };
+
+  React.useEffect(() => {
+    setIsSubmitActive(
+      Object.keys(inputs).every(key => inputs[key as keyof State].isValid)
+    );
+  }, [inputs]);
+
+  if (successMessage)
+    return (
+      <form className={c(className, classes.root, "success")} {...otherProps}>
+        <Text variant="h6" rootNode="h1" className={classes.title}>
+          ایمیل بازیابی ارسال شد!
+        </Text>
+        <Text variant="bodySmall" rootNode="p" color="textSecondary">
+          {successMessage}
+        </Text>
+        <Text
+          variant="bodySmall"
+          rootNode="p"
+          color="textSecondary"
+          className={classes.footer}
+        >
+          <Link href="/login">
+            <a title="ورود به حساب کاربری">بازگشت به ورود</a>
+          </Link>
+        </Text>
+      </form>
+    );
+
+  return (
+    <form
+      ref={ref}
+      onSubmit={e => void submit(e)}
+      className={c(className, classes.root)}
+      {...otherProps}
+    >
+      {globalError && <div className={classes.formError}>{globalError}</div>}
+      <Text variant="h6" rootNode="h1" className={classes.title}>
+        بازیابی رمز عبور
+      </Text>
+      <Text variant="bodySmall" rootNode="p" color="textSecondary">
+        ایمیل خود را جهت بازیابی رمز عبور وارد کنید:
+      </Text>
+      <FormControl
+        className={classes.formControl}
+        fluid
+        required
+        hasError={!!inputs.email.error}
+      >
+        <TextField
+          placeholder="ایمیل"
+          name="email"
+          size="large"
+          value={inputs.email.value}
+          onChange={handleTextChange}
+        />
+        {inputs.email.error && (
+          <FormControlFeedback>{inputs.email.error}</FormControlFeedback>
+        )}
+      </FormControl>
+      <Button
+        className={classes.submitBtn}
+        type="submit"
+        color="primary"
+        label="بازیابی"
+        size="large"
+        loading={isSubmitting}
+        disabled={!isSubmitActive}
+      />
+      <Text
+        variant="bodySmall"
+        rootNode="p"
+        color="textSecondary"
+        className={classes.footer}
+      >
+        <span>منصرف شدم:</span>
+        <Link href="/login">
+          <a title="ورود به حساب کاربری">بازگشت به ورود</a>
+        </Link>
+      </Text>
+    </form>
+  );
+};
+
+const ForgotPasswordForm = React.forwardRef(
+  ForgotPasswordFormBase
+) as typeof ForgotPasswordFormBase;
+
+export default ForgotPasswordForm;
