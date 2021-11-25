4cac942d5f2b8649b4768fd74f8544de559627d7
Saeed Alipoor <alipoor.sa@gmail.com>
2021-11-25 13:19:38 +0330

    [:star:] add Url component

---
+++ components/core/Url/Url.tsx
@@ -0,0 +1,85 @@
+import {
+  FormControl,
+  FormControlDescription,
+  FormControlFeedback,
+  FormControlLabel,
+  TextField
+} from "@sonnat/ui";
+import * as React from "react";
+import validateInputByBrowser from "utils/validateByBrowser";
+
+interface EmailBaseProps {
+  className?: string;
+  defaultValue?: string;
+  required?: boolean;
+}
+
+type EmailProps = Omit<
+  React.ComponentPropsWithRef<"div">,
+  keyof EmailBaseProps
+> &
+  EmailBaseProps;
+
+interface FieldState {
+  value: string;
+  error: string;
+}
+type Action =
+  | { type: "SET_VALUE"; value: string }
+  | { type: "SET_ERROR"; error: string };
+type Reducer = (prevState: FieldState, action: Action) => FieldState;
+
+const reducer: Reducer = (prevState, action) => {
+  switch (action.type) {
+    case "SET_VALUE": {
+      const { value } = action;
+      return { ...prevState, value };
+    }
+    case "SET_ERROR": {
+      const { error } = action;
+      return { ...prevState, error };
+    }
+    default:
+      return prevState;
+  }
+};
+
+const EmailBase = (props: EmailProps, ref: React.Ref<HTMLDivElement>) => {
+  const { defaultValue, required } = props;
+
+  const [state, dispatch] = React.useReducer(reducer, {
+    value: defaultValue || "",
+    error: ""
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target;
+
+    if (value !== state.value) {
+      const error = validateInputByBrowser(value, { required, type: "url" });
+      dispatch({
+        type: "SET_VALUE",
+        value
+      });
+      dispatch({
+        type: "SET_ERROR",
+        error
+      });
+    }
+  };
+
+  return (
+    <FormControl hasError={!!state.error} required={props.required} ref={ref}>
+      <FormControlLabel>Label</FormControlLabel>
+      <FormControlDescription>Description</FormControlDescription>
+      <TextField onChange={handleChange} value={state.value} type="text" />
+      {!!state.error && (
+        <FormControlFeedback>{state.error}</FormControlFeedback>
+      )}
+    </FormControl>
+  );
+};
+
+const Email = React.forwardRef(EmailBase) as typeof EmailBase;
+
+export default Email;
