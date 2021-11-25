db658736de7ce613606d80346f17daf63bafe276
mimshins <mostafa.sh.coderino@gmail.com>
2021-11-25 22:28:51 +0330

    update theme

--- theme.ts
+++ theme.ts
@@ -2,7 +2,6 @@ import createTheme from "@sonnat/ui/styles/createTheme";
 
 const theme = createTheme({
   direction: "rtl",
-  colors: { primary: "#f56468", secondary: "#27184f" },
   custom: {}
 });
 
