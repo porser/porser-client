ef61f61e2950f7d2419e1681b3bf842c9b0b642b
mimshins <mostafa.sh.coderino@gmail.com>
2022-02-26 12:37:41 +0330

    [:truck:] move PageSuspension component to /components

--- components/index.ts
+++ components/index.ts
@@ -1,4 +1,6 @@
 export * as Container from "./container";
 export * as Layout from "./layout";
-export * as Shared from "./shared";
 export * as Partial from "./partial";
+export * as Shared from "./shared";
+
+export { default as PageSuspension } from "./PageSuspension";
--- components/partial/index.ts
+++ components/partial/index.ts
@@ -1,1 +1,1 @@
-export { default as PageSuspension } from "./PageSuspension";
+export {};
