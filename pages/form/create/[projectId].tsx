6f95752698327f3f2ed291727a08fece408b2028
mimshins <mostafa.sh.coderino@gmail.com>
2022-02-26 12:22:56 +0330

    [:star:][🚧] implement FormBuilder sidebar

--- pages/form/create/[projectId].tsx
+++ pages/form/create/[projectId].tsx
@@ -1,25 +1,95 @@
+import { ChevronRight, CogO, EyeO } from "@sonnat/icons";
+import { Button, Flex, FlexItem, NoSsr, Text } from "@sonnat/ui";
 import makeStyles from "@sonnat/ui/styles/makeStyles";
-import { WithHeader } from "components/layout";
-import { GetServerSideProps } from "next";
+import { FormBuilderLayout, WithHeader } from "components/layout";
+import FormBuilderContext, {
+  INITIAL_STATE,
+  reducer,
+  type IContext
+} from "context/FormBuilderContext";
+import type { GetServerSideProps } from "next";
 import Head from "next/head";
 import * as React from "react";
-import type { Layout, NextPageWithLayout } from "types";
+import type { PageLayout, PorserNextPage } from "types";
 import { setTitleMeta } from "utils";
 
-const useStyles = makeStyles(() => ({ root: {} }), { name: "Page" });
+const useStyles = makeStyles({ root: {} }, { name: "Page" });
 
-const Page: NextPageWithLayout<{ projectId: string; projectName: string }> =
-  () => {
-    const classes = useStyles();
+const Page: PorserNextPage<{
+  projectId: string;
+  projectName: string;
+}> = () => {
+  const classes = useStyles();
 
-    return (
-      <main id="main" className={classes.root}>
-        <Head>{setTitleMeta("PORSER | ساخت فرم")}</Head>
-      </main>
-    );
-  };
+  return (
+    <main id="main" className={classes.root}>
+      <Head>{setTitleMeta("PORSER | ساخت فرم")}</Head>
+    </main>
+  );
+};
 
-const PageLayout: Layout = page => <WithHeader>{page}</WithHeader>;
+const useSubHeaderStyles = makeStyles(
+  ({ spacings: { spaces } }) => ({
+    root: {},
+    icon: { marginLeft: spaces[1].rem },
+    title: { marginRight: spaces[7].rem },
+    controls: { "& > * + *": { marginRight: spaces[3].rem } }
+  }),
+  { name: "SubHeader" }
+);
+
+const PageSubHeader = () => {
+  const classes = useSubHeaderStyles();
+
+  return (
+    <Flex
+      className={classes.root}
+      mainAxisAlignment="between"
+      crossAxisAlignment="center"
+    >
+      <FlexItem crossAxisSelfAlignment="center">
+        <Text variant="caption" as="a" href="#" color="textSecondary">
+          <ChevronRight className={classes.icon} size={16} />
+          پروژه پژوهش املاک
+        </Text>
+        <Text className={classes.title} variant="h6" as="strong">
+          عنوان پرسش‌نامه
+        </Text>
+      </FlexItem>
+      <FlexItem className={classes.controls} crossAxisSelfAlignment="center">
+        <Button
+          size="large"
+          label="تنظیمات"
+          leadingIcon={<CogO />}
+          variant="inlined"
+        />
+        <Button
+          size="large"
+          label="پیش‌نمایش"
+          leadingIcon={<EyeO />}
+          variant="inlined"
+        />
+        <Button size="large" label="انتشار" variant="filled" color="primary" />
+      </FlexItem>
+    </Flex>
+  );
+};
+
+const Layout: PageLayout = page => {
+  const [state, dispatch] = React.useReducer(reducer, INITIAL_STATE);
+
+  const context = React.useMemo<IContext>(() => ({ state, dispatch }), [state]);
+
+  return (
+    <WithHeader subHeader={<PageSubHeader />}>
+      <NoSsr>
+        <FormBuilderContext.Provider value={context}>
+          <FormBuilderLayout>{page}</FormBuilderLayout>
+        </FormBuilderContext.Provider>
+      </NoSsr>
+    </WithHeader>
+  );
+};
 
 export const getServerSideProps: GetServerSideProps = async ctx => {
   await Promise.resolve();
@@ -30,6 +100,6 @@ export const getServerSideProps: GetServerSideProps = async ctx => {
   return { props: { projectId, projectName: "پروژه اول" } };
 };
 
-Page.getLayout = () => PageLayout;
+Page.getLayout = () => Layout;
 
 export default Page;
